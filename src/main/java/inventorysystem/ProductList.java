c3fdf73a33af1caa4eaef1e4cae7be110df59ce2
LoneDespairYt <lonedespairyt@gmail.com>
2022-12-12 20:55:54 +0800

    added cart page

--- src/main/java/inventorysystem/ProductList.java
+++ src/main/java/inventorysystem/ProductList.java
@@ -149,7 +149,8 @@ public class ProductList extends javax.swing.JPanel {
         setBackground(new java.awt.Color(232, 243, 214));
 
         HeaderPanel.setBackground(new java.awt.Color(250, 171, 120));
-        HeaderPanel.setMinimumSize(new java.awt.Dimension(800, 108));
+        HeaderPanel.setMinimumSize(new java.awt.Dimension(800, 100));
+        HeaderPanel.setPreferredSize(new java.awt.Dimension(800, 100));
 
         jLabel1.setFont(new java.awt.Font("Century Gothic", 1, 36)); // NOI18N
         jLabel1.setForeground(new java.awt.Color(232, 243, 214));
@@ -170,7 +171,7 @@ public class ProductList extends javax.swing.JPanel {
             .addGroup(HeaderPanelLayout.createSequentialGroup()
                 .addGap(27, 27, 27)
                 .addComponent(jLabel1)
-                .addContainerGap(34, Short.MAX_VALUE))
+                .addContainerGap(26, Short.MAX_VALUE))
         );
 
         btnUpdate.setFont(new java.awt.Font("Century Gothic", 0, 12)); // NOI18N
@@ -354,7 +355,7 @@ public class ProductList extends javax.swing.JPanel {
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(layout.createSequentialGroup()
                 .addComponent(HeaderPanel, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
                 .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
                     .addGroup(layout.createSequentialGroup()
                         .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
@@ -405,7 +406,7 @@ public class ProductList extends javax.swing.JPanel {
                 .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
                     .addComponent(btnUpdate)
                     .addComponent(btnClear))
-                .addGap(18, 18, 18))
+                .addContainerGap(26, Short.MAX_VALUE))
         );
     }// </editor-fold>//GEN-END:initComponents
 
